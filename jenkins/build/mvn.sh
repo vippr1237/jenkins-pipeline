dca15d2edaa11a10527f9ba4086b288a49efabbc
vippr1237 <vippr1239@gmail.com>
2021-08-29 04:48:56 +0000

    fix-path

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,5 +3,6 @@
 echo "********************"
 echo "****Building jar****"
 echo "********************"
+WORKSPACE=/var/lib/jenkins/workspace/Jenkins-pipeline
 
-docker run --rm -v $PWD/devops_demo:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.8.1-adoptopenjdk-11 "$@"
+docker run --rm -v $WORKSPACE/idevops_demo:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.8.1-adoptopenjdk-11 "$@"
